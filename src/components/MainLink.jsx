c9be5e9c25dc9036d5786a4f9d4334b3a81f55ba
mertcetiin <mrtctn506@gmail.com>
2023-09-26 13:21:56 +0300

    mainLink.css half done

--- src/components/MainLink.jsx
+++ src/components/MainLink.jsx
@@ -4,14 +4,16 @@ import '../styles/MainLink.css'
 //import { FaXTwitter } from 'react-icons/fa'
 import { PiHouseSimpleFill } from 'react-icons/pi'
 import { GrSearch } from 'react-icons/gr'
+import { PiBell } from 'react-icons/pi'
+import { RiTwitterXFill } from 'react-icons/ri'
 
 
 function MainLink() {
     return (
         <div className='container'>
-            {/* <div className='linkDiv'>
-                <FaXTwitter />
-            </div> */}
+            <button className='linkDiv'>
+                <RiTwitterXFill />
+            </button>
             <button className='linkDiv'>
                 <Link to='/home'><PiHouseSimpleFill className='houseIcon' /> Anasayfa</Link>
             </button>
@@ -19,7 +21,7 @@ function MainLink() {
                 <Link to='/home'><GrSearch className='searchIcon' /> Keşfet</Link>
             </button>
             <button className='linkDiv'>
-                <Link to='/home'>Bildirimler</Link>
+                <Link to='/home'><PiBell /> Bildirimler</Link>
             </button>
             <button className='linkDiv'>
                 <Link to='/home'>Mesajlar</Link>
@@ -40,8 +42,12 @@ function MainLink() {
                 <Link to='/home'>Daha fazla</Link>
             </button>
             <button className='submitBtn'>Gönder</button>
+
+            <button className='changeBtn'>
+                Mert Çetin
+            </button>
         </div>
     )
 }
 
-export default MainLink
+export default MainLink;
